19ef8cf5893c967918d72f50d519f320b5e259d7
AbrahamJP <abrahamjosephpeter@gmail.com>
2018-03-24 18:59:46 +1100

    updated to contain relative date reference, minor fix, private function

--- src/PyDateParser.py
+++ src/PyDateParser.py
@@ -9,9 +9,13 @@ from datetime import datetime, timedelta, tzinfo
 
 class MyDateParser:
     def __init__(self):
-        self._date_string = ""
-        # shortest string will be like "1d", which is 2 chars
 
+        # this will hold the relative datetime value
+        self.__relative_datetime = None
+
+        self.__date_string = ""
+
+        # shortest string will be like "1d", which is 2 chars
         self.__min_length = 2
 
         # min and max number of tokens
@@ -20,13 +24,11 @@ class MyDateParser:
 
         self.__regexp_number = "(\d+)"
         self.__regexp_timeframe = "([s|m|h|d|w])"
-        self.__regexp_operators = "[+-]"
-        self.__operands_keywords = ["now", "yday", "tmrw", "dby", "dat"]
-        self.__regexp_operands = "\W*now\W*|" \
-                                 "\W*yday\W*|" \
-                                 "\W*tmrw\W*|" \
-                                 "\W*dby\W*" \
-                                 "\W*dat\W*"  # dat = day after tomorrow
+        self.__regexp_operators = "([+-])?"
+        self.__operand_keywords = ["now", "yday", "tmrw", "dby", "dat"]
+
+        self.__regexp_syntax = self.__regexp_operators + self.__regexp_number + \
+                               self.__regexp_timeframe
 
         self.__sample_pattern = "Syntax: enums | enums[+|-] Int [s|m|h|w] \r" \
                                 "s = seconds, m = minute, h = hour, w = week \r" \
@@ -52,14 +54,18 @@ class MyDateParser:
 
     """
 
-    def parse(self, date_string):
-        self._original_date_string = date_string
+    def parse(self, date_string, relative_datetime=None):
+
+        self._original__date_string = date_string
+
+        if relative_datetime is None:
+            self.__relative_datetime = datetime.utcnow()
 
         date_string = self.cleanup_string(date_string)
 
-        if self.is_valid(date_string):
-            tokens = self.parse_tokens(date_string)
-            return self.process_tokens(tokens)
+        if self.__is_valid(date_string):
+            tokens = self.__parse_tokens(date_string)
+            return self.__process_tokens(tokens)
         else:
             raise ValueError("Invalid date_string")
 
@@ -71,29 +77,33 @@ class MyDateParser:
     basic validation of input parameters
     """
 
-    def is_valid(self, date_string):
+    def __is_valid(self, date_string):
         if not date_string:
             raise ValueError("Empty value detected.")
 
         elif len(date_string) < self.__min_length:
             raise ValueError("Invalid string length; should have atleast {} characters.".format(self.__minlength))
 
+        elif not re.match(self.__regexp_syntax, date_string) and \
+                        date_string not in self.__operand_keywords:
+            raise ValueError("Invalid syntax.")
+
         return True
 
-    def parse_operand(self, dict_operand):
+    def __parse_operand(self, dict_operand):
 
         result = {}
 
         if dict_operand:
             # if simple operand like now, tmrw, etc
-            if dict_operand in self.__operands_keywords:
+            if dict_operand in self.__operand_keywords:
                 result["type"] = "simple"
                 result["value"] = dict_operand
 
             # complex operand, like 1s, 2d, 4s etc
             else:
-                timeframe_val = self.retrieve_timeframe(dict_operand)
-                number_val = self.retrieve_number(dict_operand)
+                timeframe_val = self.__retrieve_timeframe(dict_operand)
+                number_val = self.__retrieve_number(dict_operand)
 
                 result["type"] = "complex"
                 result["value"] = {"number": number_val,
@@ -103,7 +113,7 @@ class MyDateParser:
 
         return result
 
-    def retrieve_timeframe(self, value):
+    def __retrieve_timeframe(self, value):
 
         result = re.search(self.__regexp_timeframe, value)
         if result:
@@ -111,7 +121,7 @@ class MyDateParser:
         else:
             return None
 
-    def retrieve_number(self, value):
+    def __retrieve_number(self, value):
 
         result = re.search(self.__regexp_number, value)
         if result:
@@ -119,46 +129,59 @@ class MyDateParser:
         else:
             return None
 
-    def validate_operand(self, value):
+    def __validate_operand(self, value):
 
         complex_operand_regexp = self.__regexp_number + self.__regexp_timeframe
         if re.search(complex_operand_regexp, value):
             return True
-        elif value in self.__operands_keywords:
+        elif value in self.__operand_keywords:
             return True
         else:
             raise ValueError("Invalid format detected '{}'".format(value))
 
-    def parse_tokens(self, date_string):
+    def __parse_tokens(self, date_string):
         tokens = {}
 
-        # check the string contains operator + or -
-        if re.search(self.__regexp_operators, date_string):
+        # this matches string like -12s, 4h, 1w etc
+        if re.search(self.__regexp_syntax, date_string):
 
-            # get the positions where the text where found
+            # see if the + or - operator is present
             result = re.search(self.__regexp_operators, date_string)
 
-            # get the operator defined in the string
-            operator_start_pos = result.start()
-            operator_end_pos = result.end()
+            # if string contains -12h or +12h get the operator
+            if result and (result.start() != 0 and result.end() != 0):
+                # get the operator defined in the string
+                operator_start_pos = result.start()
+                operator_end_pos = result.end()
 
-            tokens["operator"] = date_string[operator_start_pos:operator_end_pos]
-            operand1 = date_string[0:operator_start_pos]
-            operand2 = date_string[operator_end_pos:]
+                tokens["operator"] = date_string[operator_start_pos:operator_end_pos]
 
-            if self.validate_operand(operand1):
+                # read from where the operator char ends to all the way to end of string
+                operand2 = date_string[operator_end_pos:]
+            else:
+                # if not operator specified default to +
+                tokens["operator"] = "+"
+
+                # as the input value doesn't contain operator consider entire input as second operand
+                operand2 = date_string
+
+            # set first operand always defaults to "now" as values passed like -12h, 1h, 3w
+            # will always be added or subtracted against current datetime
+            operand1 = "now"
+
+            if self.__validate_operand(operand1):
                 # get the first operand, 0 to position where the operator starts
-                tokens["operand1"] = self.parse_operand(date_string[0:operator_start_pos])
+                tokens["operand1"] = self.__parse_operand(operand1)
 
-            if self.validate_operand(operand2):
+            if self.__validate_operand(operand2):
                 # get the second operand, position where the operator end till end of string
-                tokens["operand2"] = self.parse_operand(date_string[operator_end_pos:])
+                tokens["operand2"] = self.__parse_operand(operand2)
 
-        # check the string contains tokens including now, yday, tmrw etc
-        elif re.search(self.__regexp_operands, date_string):
+        # check for string like now, yday, tmrw etc
+        elif date_string in self.__operand_keywords:
 
-            if self.validate_operand(date_string):
-                tokens["operand1"] = self.parse_operand(date_string)
+            if self.__validate_operand(date_string):
+                tokens["operand1"] = self.__parse_operand(date_string)
 
         # throw invalid token exception
         else:
@@ -166,9 +189,9 @@ class MyDateParser:
 
         return tokens
 
-
-    #{'operator': '-', 'operand1': {'type': 'simple', 'value': 'now'}, 'operand2': {'type': 'complex', 'value': {'number': '1', 'timeframe': 'd'}}}
-    def process_tokens(self, tokens):
+    # receives value of
+    # {'operator': '-', 'operand1': {'type': 'simple', 'value': 'now'}, 'operand2': {'type': 'complex', 'value': {'number': '1', 'timeframe': 'd'}}}
+    def __process_tokens(self, tokens):
 
         global operator
         ops = {"+": operator.add,
@@ -178,20 +201,21 @@ class MyDateParser:
         if "operator" in tokens:
             operator = tokens["operator"]
 
-            operand1 = self.process_operand(tokens["operand1"])
-            operand2 = self.process_operand(tokens["operand2"])
+            operand1 = self.__process_operand(tokens["operand1"])
+            operand2 = self.__process_operand(tokens["operand2"])
 
             return ops[operator](operand1, operand2)
 
         # means the token contain only single operand
         elif "operand1" in tokens:
-            return self.process_operand(tokens["operand1"])
+            return self.__process_operand(tokens["operand1"])
 
         else:
             raise ValueError("Invalid Tokens")
 
-    def process_operand(self, operand):
-        now = datetime.utcnow()
+    def __process_operand(self, operand):
+
+        now = self.__relative_datetime
 
         if "type" in operand and operand["type"] == "simple":
 
@@ -211,18 +235,13 @@ class MyDateParser:
 
             complex_vals = {
                 "s": timedelta(seconds=val),
-                "m": timedelta(seconds=60*val),
-                "h": timedelta(seconds=3600*val),
+                "m": timedelta(seconds=60 * val),
+                "h": timedelta(seconds=3600 * val),
                 "d": timedelta(days=val),
-                "w": timedelta(days=7*val)
+                "w": timedelta(days=7 * val)
             }
 
             return complex_vals[operand["value"]["timeframe"]]
 
         else:
             raise ValueError("Invalid operand")
-
-
-
-obj = MyDateParser()
-print obj.parse("now-2w")
